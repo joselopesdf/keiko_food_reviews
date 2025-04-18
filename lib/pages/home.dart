4693b8c963a8c17106bf37643a7aa5f68229ef8a
Jose Veiga <josemikissi@gmail.com>
2025-04-18 21:12:48 -0100

    Added the responsive  layout builder widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart' ;
-
+import 'package:keiko_food_reviews/pages/review_grid_photos.dart';
+import 'package:keiko_food_reviews/pages/review_list/review_list.dart';
+import 'package:keiko_food_reviews/pages/review_map_locations/review_map_locations.dart';
+import 'package:keiko_food_reviews/widget/responsive_layout_builder.dart';
 
     class Home extends StatefulWidget {
       const Home({super.key});
@@ -10,15 +13,78 @@ import 'package:flutter/material.dart' ;
 }
 
 class _HomeState extends State<Home> {
+       int _currentPageIndex = 0;
+
+      final _bodySelectedPage = <Widget>[
+        const ReviewList(),
+        const ReviewGridPhotos(),
+        const ReviewMapLocations(),
+
+      ];
       @override
       Widget build(BuildContext context) {
-        return  Scaffold(
-          appBar: AppBar(
-            title : const Text('Keiko food Reviews')
+        return  ResponsiveLayoutBuilder(
+            mobile: Scaffold(
+              body: _bodySelectedPage[_currentPageIndex],
+              bottomNavigationBar:  NavigationBar(
+                  destinations: const [
 
-          ),
+                    NavigationDestination( icon: Icon(Icons.reviews), label: 'Reviews'),
+                    NavigationDestination( icon: Icon(Icons.photo_library), label: 'Photos'),
+                    NavigationDestination( icon: Icon(Icons.location_pin), label: 'Locations')
+
+                  ] ,
+              selectedIndex: _currentPageIndex,
+              onDestinationSelected: (int index ){
+                    setState(() {
+                      _currentPageIndex = index ;
+                    });
+              },),
+            ),
+            webDesktopTablet: Scaffold(
+              body : Row(
+
+                children: [
+
+                  Container(
+                    decoration:  BoxDecoration(
+                      gradient: LinearGradient(
+                        colors:  [
+                          Theme.of(context).colorScheme.primary,
+                          Theme.of(context).colorScheme.secondaryContainer,
+                        ]
+
+                      ),
+
+                    ),
+                    child:  NavigationRail(
+                      selectedIndex:  _currentPageIndex,
+                      onDestinationSelected: (int index){
+                        _currentPageIndex = index ;
+                      },
+                      destinations: const [
+                        NavigationRailDestination(icon: Icon(Icons.reviews), label: Text('Reviews')),
+                        NavigationRailDestination(icon: Icon(Icons.photo_library), label: Text('Photos')),
+                        NavigationRailDestination(icon: Icon(Icons.location_pin), label: Text('Locations')),
+
+                      ],
+                    backgroundColor: Colors.transparent,
+                        labelType: NavigationRailLabelType.all,
+                        leading: Padding(
+
+                            padding: const EdgeInsets.only( bottom: 24.0),
+                          child: Image.asset('assets/images/crown.png',width: 48.0,fit: BoxFit.cover,),
+                        ),
+                      
+                  )
+
+                  ),
+                Expanded(child: _bodySelectedPage[_currentPageIndex])],
+
+              )
+
+            )) ;
 
 
-        );
       }
 }

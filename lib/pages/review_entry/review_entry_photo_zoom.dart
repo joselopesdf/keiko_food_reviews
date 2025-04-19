c01d844a31b588feecd11b2236eee903bbe28566
Jose Veiga <josemikissi@gmail.com>
2025-04-19 18:52:16 -0100

    Added authentication and review_entry pages

---
+++ lib/pages/review_entry/review_entry_photo_zoom.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart' ;
+
+class ReviewEntryPhotoZoom extends StatelessWidget {
+  const ReviewEntryPhotoZoom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

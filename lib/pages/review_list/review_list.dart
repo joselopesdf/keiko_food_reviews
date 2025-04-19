65ca7bacc0d17ce542e8dc2e5b1240afebd9db0b
Jose Veiga <josemikissi@gmail.com>
2025-04-19 19:38:11 -0100

    add appBar with custom clipper

--- lib/pages/review_list/review_list.dart
+++ lib/pages/review_list/review_list.dart
@@ -1,6 +1,6 @@
 
-
 import 'package:flutter/material.dart' ;
+import 'package:keiko_food_reviews/helper/clippers.dart';
 
 class ReviewList extends StatelessWidget {
 
@@ -9,6 +9,30 @@ class ReviewList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Keiko Reviews'),
+        backgroundColor: Colors.transparent,
+        elevation: 2.0,
+        flexibleSpace: ClipPath(
+          clipper: OvalClipperUpper(),
+          child: Container(
+            decoration:  BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).colorScheme.primary,
+                    Theme.of(context).colorScheme.secondaryContainer
+                  ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter )
+            ),
+          ),
+
+        ) ,
+
+      ),
+      extendBodyBehindAppBar: true,
+
+    );
   }
 }
